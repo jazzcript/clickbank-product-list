a79921c87331938e918c76a0c70b269b13da7aff
jazzcript <jazzcript@hotmail.com>
2020-12-30 01:04:23 -0300

    clean

--- index.js
+++ index.js
@@ -3,57 +3,53 @@ const webzip = require('webzip');
 const xmlParser = new xml2js.Parser({ attrkey: "ATTR" });
 
 function cleanData(data) {
-    let count = 0;
-    let categories = data;
-    for (let category of categories) {
-        category['Name'] = category['Name'][0];
+	let categories = data;
+	for (let category of categories) {
+		category['Name'] = category['Name'][0];
 
-        if (category['Site']) {
-            for (let product of category['Site']) {
-                let keys = Object.keys(product);
-                for (let key of keys) {
-                    product[key] = product[key][0];
-                }
-            }
+		if (category['Site']) {
+			for (let product of category['Site']) {
+				let keys = Object.keys(product);
+				for (let key of keys) {
+					product[key] = product[key][0];
+				}
+			}
 
-            category['Products'] = category['Site'];
+			category['Products'] = category['Site'];
 
-            count += category['Products'].length;
-            delete category['Site'];
+			delete category['Site'];
 
-        } else {
-            category['Products'] = [];
-        }
+		} else {
+			category['Products'] = [];
+		}
 
-        if (category['Category']) {
-            delete category['Category'];
-        }
-    }
+		if (category['Category']) {
+			delete category['Category'];
+		}
+	}
 
-    return data;
+	return data;
 }
 
 function xmlToJson(data) {
-    var products;
+	var products;
 
-    xmlParser.parseString(data, function(error, result) {
-        if(error === null) {
-            products = cleanData(result['Catalog']['Category']);
-            
-        } else {
-            console.log(error);
-        }
-    });
+	xmlParser.parseString(data, function(error, result) {
+		if(error === null) {
+			products = cleanData(result['Catalog']['Category']);
+			
+		} else {
+			console.log(error);
+		}
+	});
 
-    return products;
+	return products;
 }
 
 function clickbankProductList(xmlZipUrl, targetXml) {
-	return new Promise(function (resolve, reject) {
-		xmlZipUrl = xmlZipUrl || 'https://accounts.clickbank.com/feeds/marketplace_feed_v2.xml.zip';
-		targetXml = targetXml || 'marketplace_feed_v2.xml';
-		webzip(xmlZipUrl, targetXml).then(xmlToJson).then(resolve);
-	});
+	xmlZipUrl = xmlZipUrl || 'https://accounts.clickbank.com/feeds/marketplace_feed_v2.xml.zip';
+	targetXml = targetXml || 'marketplace_feed_v2.xml';
+	return webzip(xmlZipUrl, targetXml).then(xmlToJson);
 }
 
 module.exports = clickbankProductList;
